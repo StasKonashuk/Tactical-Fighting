07bdb3af982e0d325098784ac101d4869c9e7a94
Stanislav <skonashuk@list.ru>
2022-02-07 17:25:01 +0300

    Create unitInfo component with info about health and damage value

---
+++ src/components/Board/UnitCard/UnitInfo/UnitInfo.tsx
@@ -0,0 +1,34 @@
+import { UnitDamageType } from '../../../../units/units-types/damage-type/UnitDamageType';
+import { UnitHealth } from './UnitHealth/UnitHealth';
+import { UnitDamageValue } from './UnitDamageValue/UnitDamageValue';
+import { ReactElement } from 'react';
+import style from './UnitInfo.module.css';
+
+interface IUnitInfoProps {
+  hp: number;
+  name: string;
+  damageValue: number;
+  unitDamageType: UnitDamageType;
+  isDead: boolean;
+  startHp: number;
+}
+
+export const UnitInfo = ({
+  hp,
+  startHp,
+  name,
+  damageValue,
+  unitDamageType,
+  isDead,
+}: IUnitInfoProps): ReactElement => {
+  return (
+    <div className={style.userInfoContainer}>
+      <UnitDamageValue
+        damageValue={damageValue}
+        unitDamageType={unitDamageType}
+      />
+      <span className={style.unitName}>{name}</span>
+      <UnitHealth isDead={isDead} hp={hp} startHp={startHp} />
+    </div>
+  );
+};
