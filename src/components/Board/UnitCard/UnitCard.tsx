87bcd5c607132babe121b07140cdf64e6d64bdbb
Stanislav <skonashuk@list.ru>
2022-02-07 17:25:45 +0300

    Create unitCard component

---
+++ src/components/Board/UnitCard/UnitCard.tsx
@@ -0,0 +1,59 @@
+import { ReactElement } from 'react';
+import { Unit } from '../../../units/Unit';
+import classNames from 'classnames';
+import { UnitImage } from './UnitImage/UnitImage';
+import { UnitInfo } from './UnitInfo/UnitInfo';
+import style from './UnitCard.module.css';
+import { team } from '../../../types/types';
+
+interface UnitCardPropsType {
+  unit: Unit;
+  isDead: boolean;
+  isDefending: boolean;
+  isParalyzing: boolean;
+  isCurrent: boolean;
+  isTarget: boolean;
+  unitTeam: string;
+  handleSelectTarget: (arg: Unit) => void;
+}
+
+export const UnitCard = ({
+  unit,
+  isDead,
+  isDefending,
+  isParalyzing,
+  isCurrent,
+  isTarget,
+  unitTeam,
+  handleSelectTarget,
+}: UnitCardPropsType): ReactElement => {
+  return (
+    <div
+      onClick={() => handleSelectTarget(unit)}
+      className={classNames(style.unitItem, {
+        [style.current]: isCurrent,
+        [style.target]: isTarget,
+        [style.topTeam]: unitTeam === 'TOP_TEAM',
+        [style.bottomTeam]: unitTeam === 'BOTTOM_TEAM',
+      })}
+    >
+      <UnitImage
+        unitTeam={unitTeam as team}
+        gameRound={1}
+        name={unit.getName()}
+        isOrder={false}
+        isDead={isDead}
+        isParalyzing={isParalyzing}
+        isDefending={isDefending}
+      />
+      <UnitInfo
+        isDead={isDead}
+        name={unit.getName()}
+        hp={unit.getHealth()}
+        startHp={unit.getMaxHealth()}
+        damageValue={unit.getDamageValue()}
+        unitDamageType={unit.getUnitType()}
+      />
+    </div>
+  );
+};
