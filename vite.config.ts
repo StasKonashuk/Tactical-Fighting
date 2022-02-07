1224c5b16fa34612c373670188c94bc7a74156a6
Stanislav <skonashuk@list.ru>
2022-02-07 18:57:54 +0300

    Fix ts and vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
+import path from 'path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Tactical-Fighting/',
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+  resolve: {
+    alias: {
+      stream: 'stream-browserify',
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
